91965a895748ce641e4b9006f049227d4ef9198d
Anton <antonkarabeinikov@gmail.com>
2022-06-16 19:56:26 +0200

    Update house.py #3

--- index/house.py
+++ index/house.py
@@ -32,8 +32,7 @@ class House:
             return True
         if self.age >= 10:
             return False
-        default:
-            return False;
+        return False;
 
 
 if __name__ == '__main__':
